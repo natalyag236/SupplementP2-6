848de36d9897e929ecbb8815eb52c09af9705cad
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-12-05 21:56:06 -0500

    test: updated test case for sorting a list in ascending order

--- TestascendingSort.java
+++ TestascendingSort.java
@@ -1,13 +1,12 @@
 import static org.junit.Assert.*;
-import java.sql.Array;
-import java.util.List;
+import java.util.*;
 import org.junit.Test;
 
 public class TestascendingSort {
     @Test
     public void testSortListAscending (){
-        List<Integer> unsortedList = Array.asList(5, 2, 8, 1, 4);
-        List<Integer> expectedSortedList = Array.asList(1, 2, 4, 5, 8);
+        List<Integer> unsortedList = Arrays.asList(5, 2, 8, 1, 4);
+        List<Integer> expectedSortedList = Arrays.asList(1, 2, 4, 5, 8);
 
         List<Integer> sortedList = ListSorter.sortAscending(unsortedList);
 
