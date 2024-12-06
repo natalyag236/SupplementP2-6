e6d25c25569aedd06542c2d36ee2190789ee1a2d
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-12-05 21:28:48 -0500

    doc: add documentation for integer counter class and count count occurrences method

--- IntegerCounter.java
+++ IntegerCounter.java
@@ -2,9 +2,17 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * A utility class for counting occurrences of integers in a list.
+ */
 
 public class IntegerCounter {
-
+    /**
+     * Counts the occurrences of each integer in a list.
+     *
+     * @param numbers The list of integers to process.
+     * @return A map where each key is an integer, and each value is the number of occurrences.
+     */
     public static Map<Integer, Integer> countOccurrences(List<Integer> numbers) {
         Map<Integer, Integer> frequencyMap = new HashMap<>();
 
