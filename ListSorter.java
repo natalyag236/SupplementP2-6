bbcd063f1f28b986206e993abae7f6882309e6b2
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-12-05 22:17:18 -0500

    doc: add documentation for list sorter class and sortAscending method

--- ListSorter.java
+++ ListSorter.java
@@ -3,6 +3,12 @@ import java.util.Collections;
 import java.util.List;
 
 public class ListSorter {
+    /**
+     * Sorts a list of integers in ascending order.
+     *
+     * @param numbers The list of integers to sort.
+     * @return A new list sorted in ascending order.
+     */
     public static List<Integer> sortAscending(List<Integer> numbers) {
         List<Integer> sortedList = new ArrayList<>(numbers);
 
