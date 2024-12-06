65f686301693444c6d46cb053a37d4fe07778f4c
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-12-05 20:51:29 -0500

    test: add documentation to random list generator

--- RandomListGenerator.java
+++ RandomListGenerator.java
@@ -3,6 +3,13 @@ import java.util.List;
 import java.util.Random;
 
 public class RandomListGenerator {
+    /**
+     * Generates a list of random integers.
+     *
+     * @param size The number of integers to generate in the list.
+     * @return A list containing random integers between 0 and 99.
+     * @throws IllegalArgumentException if the size is negative.
+     */
     public static List<Integer> generateRandomList(int size) {
         List<Integer> randomList = new ArrayList<>();
         Random random = new Random();
